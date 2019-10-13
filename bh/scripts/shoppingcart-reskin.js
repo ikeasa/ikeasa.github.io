95b7c0b2f29cde9ea0e1cdf4fd31933e05e52d29
Hamed Alkaf <A07558@ikea.com.sa>
2019-10-13 10:07:43 +0300

    fix Bahrain profile link

--- bh/scripts/shoppingcart-reskin.js
+++ bh/scripts/shoppingcart-reskin.js
@@ -68,7 +68,7 @@
   
         '<ul class="header__icon-list header__icon-count-3">' +
   
-        '<li class="header__icon-list-item"><a class="header__icon-list-item-link" href="' + __('https://secure.ikea.com/webapp/wcs/stores/servlet/LogonForm?storeId=39&langId=-49') + '"><div class="header__icon-list-item-icon">' +
+        '<li class="header__icon-list-item"><a class="header__icon-list-item-link" href="' + __('https://www.ikea.com/bh/en/profile/#/login') + '"><div class="header__icon-list-item-icon">' +
         '<svg class="hnf-svg-icon" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15,12H9a5,5,0,0,0-5,5v4H20V17A5,5,0,0,0,15,12Zm3,7H6V17a3,3,0,0,1,3-3h6a3,3,0,0,1,3,3Zm-6-8A4,4,0,1,0,8,7,4,4,0,0,0,12,11Zm0-6a2,2,0,1,1-2,2A2,2,0,0,1,12,5Z"></path></svg>' +
         '</div><span class="header__icon-list-item-text">' + __('My profile') + '</span></a></li>' +
   
