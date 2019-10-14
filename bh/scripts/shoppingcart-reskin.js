78d6d3858c40d8a6e15036cd61fcfb294a89e272
Hamed Alkaf <A07558@ikea.com.sa>
2019-10-14 09:29:44 +0300

    fix bahrain shopping bag link

--- bh/scripts/shoppingcart-reskin.js
+++ bh/scripts/shoppingcart-reskin.js
@@ -76,7 +76,7 @@
         '<svg class="hnf-svg-icon" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M16,14l2-2V22H2V2H16L14,4H4V20H16ZM12,6H6V8h6ZM6,12H8V10H6Zm-.23,6.29,4.66-2.05L22,4.66,19.34,2,7.72,13.73Z"></path></svg>' +
         '</div><span class="header__icon-list-item-text">' + __('Shopping list') + '</span></a></li>' +
   
-        '<li class="header__icon-list-item"><a class="header__shopping-cart header__icon-list-item-link" href="https://' + www + '.ikea.com/bh/' + lang + '/mcommerce/shoppingcart"><div class="header__icon-list-item-icon js-shopping-cart-icon" data-market-code="en-BH">' +
+        '<li class="header__icon-list-item"><a class="header__shopping-cart header__icon-list-item-link" href="https://secure.ikea.com/bh/en/mcommerce/shoppingcart"><div class="header__icon-list-item-icon js-shopping-cart-icon" data-market-code="en-BH">' +
         '<svg class="hnf-svg-icon" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M16.677 10l-1.245-3.114L12.646 5h-1.292L8.568 6.886 7.323 10H2l2.544 7.633A2 2 0 0 0 6.441 19h11.117a2 2 0 0 0 1.898-1.368L22 10zm-6-3h2.646l1.2 3H9.477zm6.881 10H6.441l-1.666-5h14.45z"></path></svg>' +
         '<span class="header__shopping-bag-quantity" style="display: none"></span></div><span class="header__icon-list-item-text">' + __('Shopping bag') + '</span></a></li>' +
         
